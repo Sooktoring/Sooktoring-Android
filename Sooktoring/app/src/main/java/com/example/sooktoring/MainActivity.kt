252118fac273e729bdf709b56888d9c1c27a40ea
ofzlo <abcd2fghijklm@sookmyung.ac.kr>
2022-08-05 18:57:30 +0900

    FEAT : Login

--- Sooktoring/app/src/main/java/com/example/sooktoring/MainActivity.kt
+++ Sooktoring/app/src/main/java/com/example/sooktoring/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.sooktoring
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import com.google.android.material.tabs.TabLayoutMediator
@@ -10,18 +11,26 @@ import com.example.sooktoring.FragmentContest
 import com.example.sooktoring.FragmentGroup
 import com.example.sooktoring.FragmentMentoring
 import com.example.sooktoring.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_mentoring.*
 
 
-class MainActivity : FragmentActivity() {
+class MainActivity : FragmentActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+    var bundle = Bundle()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        //batoomnavigation bar fragment 이동
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(this)
+        //set default screen
+        binding.bottomNavigation.selectedItemId = R.id.homeFragment
+
+        //tab layout
         val strings = listOf("멘토링","동아리","공모전")
 
         val fragmentList = listOf(FragmentMentoring(), FragmentGroup(), FragmentContest())
@@ -34,6 +43,28 @@ class MainActivity : FragmentActivity() {
         TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             tab.text = tabTitles[position]
         }.attach()
+    }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.homeFragment->{
+                var homeViewFragment = FragmentHome()
+                homeViewFragment.arguments = bundle
+                supportFragmentManager.beginTransaction().replace(R.id.main_container, homeViewFragment).commit()
+                return true
+            }
+            R.id.chatFragment->{
+                var chatViewFragment = FragmentChat()
+                chatViewFragment.arguments = bundle
+                supportFragmentManager.beginTransaction().replace(R.id.main_container, chatViewFragment).commit()
+                return true
+            }
+            R.id.myPageFragment->{
+                var mypageViewFragment = FragmentMy()
+                mypageViewFragment.arguments = bundle
+                supportFragmentManager.beginTransaction().replace(R.id.main_container, mypageViewFragment).commit()
+            }
+        }
+        return false
     }
 }
--- Sooktoring/app/src/main/java/com/example/sooktoring/login/LoginState.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sooktoring.login
-
-sealed class LoginState<out R> {
-    data class Success<out T>(val data: T): LoginState<T>()
-    data class Error(val exception: Exception): LoginState<Nothing>()
-}
