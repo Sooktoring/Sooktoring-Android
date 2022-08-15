9fffeb228287d1cbb6d3ac7c4bc002a58c3b7559
Yoorhim Cho <90624848+ofzlo@users.noreply.github.com>
2022-08-16 00:35:55 +0900

    feat: initial profile layout

--- Sooktoring/app/src/main/java/com/example/sooktoring/MainActivity.kt
+++ Sooktoring/app/src/main/java/com/example/sooktoring/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sooktoring
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 import androidx.fragment.app.FragmentActivity
 import com.google.android.material.tabs.TabLayoutMediator
@@ -12,6 +13,7 @@ import com.example.sooktoring.home.FragmentContest
 import com.example.sooktoring.home.FragmentGroup
 import com.example.sooktoring.home.FragmentMentoring
 import com.example.sooktoring.databinding.ActivityMainBinding
+import com.example.sooktoring.initProfile.FragmentInitProfileSetting
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_mentoring.*
@@ -23,6 +25,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     private lateinit var homeFragment: FragmentHome
     private lateinit var chatFragment: FragmentChat
     private lateinit var myFragment: FragmentMy
+    private lateinit var initProfileFragment : FragmentInitProfileSetting
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,8 +50,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 supportFragmentManager.beginTransaction().replace(R.id.frame_fragments, chatFragment).commit()
             }
             R.id.menu_my -> {
-                myFragment = FragmentMy.newInstance()
-                supportFragmentManager.beginTransaction().replace(R.id.frame_fragments, myFragment).commit()
+                initProfileFragment = FragmentInitProfileSetting.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_fragments, initProfileFragment).commit()
+//                myFragment = FragmentMy.newInstance()
+//                supportFragmentManager.beginTransaction().replace(R.id.frame_fragments, myFragment).commit()
             }
         }
         return true
